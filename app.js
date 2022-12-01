0fd3e36d89c45ed27f85452de53cab95a1fa7459
FrancoWork <franco_ruggiero@carrefour.com>
2022-12-01 16:00:26 -0300

    newStyles

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-const express = require("express");
+const express   = require('express');
+const path      = require('path');
+const app       = express();
+// const { append } = require("express/lib/response");
+
+const port      = 5500;
+
+app.listen( port, () => {
+
+    console.log( `Funciona el servidor` );
+});
+
+app.use(express.static("/public"));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('views/home');
+});
--- routes/routesController.js
+++
@@ -1,6 +0,0 @@
-
-
-app.get("/", function(req, res) {
-    res.send('Hello World!');
-});
-
